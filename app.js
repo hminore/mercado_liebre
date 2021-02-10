785a9c0718efdead30a0839407543811c5723192
hminore <minorehernan@gmail.com>
2021-02-10 17:03:54 -0300

    Procfile y app.js adaptado para Heroku

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
+const path = require('path');
 const express = require('express');
 const app = express();
-app.use(express.static('public'));
 
+//  para ver un Jason con los datos que se envían a una página
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.listen(3000, ()=>{
-    console.log('Servidor funcionando');
-});
+// informo que la carpeta public va a ser estatica (público)
+//app.use(express.static('public'));
+publicPath = path.resolve(__dirname, './public');
+app.use( express.static(publicPath));
 
+// Levantando el servidor para que escuche en el puerto 3000
+// app.listen(3000, ()=>{
+//     console.log('Servidor funcionando en puerto 3000');
+// });
+app.listen(process.env.PORT || 3000, function(){
+    console.log('Servidor funcionando en puerto 3000');
+})
+
+// enviado una pagina HTML con la ruta de Root. (http://localhost:3000)
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
 });
@@ -18,3 +31,9 @@ app.get('/login', (req,res)=>{
 app.get('/register', (req,res)=>{
     res.sendFile(__dirname + '/views/register.html');
 });
+
+//  para ver un Jason con los datos que se envían por post a la pagina root (bodyParser)
+ app.post("/", function(req, res){
+     res.send(req.body);  // el body tiene todas las variables que le mandamos
+
+    });
